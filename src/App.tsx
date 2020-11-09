6047a96ec31aeb89980b11af07173a11049f4d89
Juliano Costa <apfjuliano@gmail.com>
2020-11-08 21:29:02 -0300

    Properties (props) - Lesson 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from './components/Header'
+import Menu from './components/Menu'
 
 function App() {
     return (
         <>
-            <Header />  
+            <Header title="Página inicial"/>  
             <h1>Hello Dev!</h1>
+            <Menu>
+                <ul>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>About</li>
+                </ul>    
+            </Menu> 
         </>
     );
 }
