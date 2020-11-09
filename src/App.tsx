7b5124d184dea1abbc1d15ec704058ca757d5f8c
Juliano Costa <apfjuliano@gmail.com>
2020-11-08 21:20:53 -0300

    Components - Lesson 3

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header'
+
+function App() {
+    return (
+        <>
+            <Header />  
+            <h1>Hello Dev!</h1>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>Cabeçalho</header>
+    )
+}
+
+export default Header
