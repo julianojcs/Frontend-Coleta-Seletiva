e8a30d612df3a4e2040d3116510c434036fc2026
Juliano Costa <apfjuliano@gmail.com>
2020-11-11 00:46:08 -0300

    Conceitos de Estado e Imutabilidade - Lesson 05

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header'
 import Menu from './components/Menu'
 
 function App() {
+    const [counter, setCounter] = useState(1)
+
+    React.useEffect(() => {
+        console.log(counter)
+    }, [counter]);
+
+    const handleButtonClick = () => {
+        setCounter(counter + 1)
+        setTimeout(() => {
+            console.log(counter)
+        }, 2000);
+    }
+    const handleButtonClick2 = () => {
+        console.log(counter)
+    }
     return (
         <>
             <Header title="Página inicial"/>  
             <h1>Hello Dev!</h1>
+            <p>{ counter }</p>
+            <button onClick={handleButtonClick}>Incrementar</button>
+            <button onClick={handleButtonClick2}>imprimir</button>
             <Menu>
                 <ul>
                     <li>Home</li>
