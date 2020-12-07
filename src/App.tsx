01d6c32b4fe95be399670bc623acf33418f8bfe6
Juliano Costa <apfjuliano@gmail.com>
2020-12-06 23:28:42 -0300

    Página Home da SPA React - Lesson 06

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
-import React, {useState} from 'react';
-import Header from './components/Header'
-import Menu from './components/Menu'
+import React from 'react'
+import './App.css'
+
+import Routes from './routes'
 
 function App() {
-    const [counter, setCounter] = useState(1)
-
-    React.useEffect(() => {
-        console.log(counter)
-    }, [counter]);
-
-    const handleButtonClick = () => {
-        setCounter(counter + 1)
-        setTimeout(() => {
-            console.log(counter)
-        }, 2000);
-    }
-    const handleButtonClick2 = () => {
-        console.log(counter)
-    }
     return (
         <>
-            <Header title="Página inicial"/>  
-            <h1>Hello Dev!</h1>
-            <p>{ counter }</p>
-            <button onClick={handleButtonClick}>Incrementar</button>
-            <button onClick={handleButtonClick2}>imprimir</button>
-            <Menu>
-                <ul>
-                    <li>Home</li>
-                    <li>Contact</li>
-                    <li>About</li>
-                </ul>    
-            </Menu> 
+            <Routes />
         </>
     );
 }
---
+++ src/pages/CreateLocation/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import './style.css'
+
+const CreateLocation: React.FC = () => {
+    return (
+        <h1>Create Location</h1>
+    )
+}
+
+export default CreateLocation
