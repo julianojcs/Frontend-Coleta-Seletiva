dbc6c4621dc8203740eec18b05cc0047d1702e83
Juliano Costa <apfjuliano@gmail.com>
2020-12-08 23:44:59 -0300

    Create Location - Part 2 - Lesson 08

--- src/pages/CreateLocation/index.tsx
+++ src/pages/CreateLocation/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker} from 'react-leaflet';
@@ -14,28 +14,72 @@ interface Item {
 }
 
 const CreateLocation: React.FC = () => {
-    const [items, setItems] = useState<Item[]>([])
+    const [items, setItems] = useState<Item[]>([]);
 
     const [selectedMapPosition, setSelectedMapPosition] = useState<[number, number]>([0, 0]);
 
-    const handleInputChange = () => {
-        
-    }
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        whatsapp: '',
+        city: '',
+        uf: '',
+    });
+
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
     useEffect(() => {
         api.get('items').then(response => {
             setItems(response.data);
-            console.log(response);
-        })
-    }, [])
+        });
+    }, []);
 
     const handleMapClick = useCallback((event: LeafletMouseEvent): void => {
-        console.log(event);
+        //console.log(event);
         setSelectedMapPosition([
             event.latlng.lat,
             event.latlng.lng,
         ]);
     }, []);
+
+    const handleInputChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        //console.log(event.target.name, event.target.value);
+        const { name, value } = event.target;
+        setFormData({...formData, [name]: value });
+    }, [formData]);
+
+    const handleSelectItem = useCallback((id: number) => {
+        //setSelectedItems([...selectedItems, id]);
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if (alreadySelected >= 0) {
+            const filteredItems = selectedItems.filter(item => item !== id);
+            setSelectedItems(filteredItems);
+        } else {
+            setSelectedItems([...selectedItems, id]);
+        }
+    }, [selectedItems]);
+
+    const handleSubmit = useCallback(async (event: FormEvent) => {
+        event.preventDefault();
+        
+        const {city, email, name, uf, whatsapp} = formData;
+        const [latitude, longitude] = selectedMapPosition;
+        const items = selectedItems;
+
+        const data = {
+            city,
+            email,
+            name,
+            uf,
+            whatsapp,
+            latitude,
+            longitude,
+            items,
+        };
+
+        await api.post('locations', data);
+    }, [formData, selectedItems, selectedMapPosition]);
 
     return (
         <div id="page-create-location">
@@ -48,7 +92,7 @@ const CreateLocation: React.FC = () => {
                     </Link>
                 </header>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h1>Cadastro do <br /> local de coleta</h1>
 
                     <fieldset>
@@ -127,7 +171,11 @@ const CreateLocation: React.FC = () => {
                         </legend>
                         <ul className="items-grid">
                             {items.map(item => (
-                                <li key={item.id}>
+                                <li
+                                    key={item.id}
+                                    onClick={() => handleSelectItem(item.id)}
+                                    className={selectedItems.includes(item.id) ? 'selected' : ''}
+                                >
                                     <img src={item.image_url} alt={item.title} />
                                 </li>
                             ))}
@@ -140,7 +188,7 @@ const CreateLocation: React.FC = () => {
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
-export default CreateLocation
+export default CreateLocation;
